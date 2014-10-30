17c4d700d3f36984961a94df70f6ee8ed73b1cfc
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2014-10-30 16:41:47 +0000

    Return cigar

--- bamread.py
+++ bamread.py
@@ -1,3 +1,4 @@
+import operator
 import numpy as np
 import pysam
 
@@ -9,12 +10,15 @@ def fetch(bam_file, chrom, start, end):
         positions = []
         lengths = []
         seqs = ''
+        cigars = []
         for read in reads:
             positions.append(read.pos)
             lengths.append(read.rlen)
             seqs += read.seq
+            cigars += [val for step in read.cigar for val in step] + [-1, -1]
         return {
             'pos': np.asarray(positions, dtype=np.uint32),
             'len': np.asarray(lengths, dtype=np.uint16),
-            'seq': np.asarray([seqs], dtype='S'+str(len(seqs)))
+            'seq': np.asarray([seqs], dtype='S'+str(len(seqs))),
+            'cigar': np.asarray(cigars, dtype=np.int16),
         }.items()
