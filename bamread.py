90759f3d286c827e67ed74ec0b0d546aa32d0912
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2014-10-31 16:33:28 +0000

    Consider reads with empty cigars to be unmapped

--- bamread.py
+++ bamread.py
@@ -12,10 +12,11 @@ def fetch(bam_file, chrom, start, end):
         seqs = ''
         cigars = []
         for read in reads:
-            positions.append(read.pos)
-            lengths.append(read.rlen)
-            seqs += read.seq
-            cigars += [val for step in read.cigar for val in step] + [-1, -1]
+            if read.cigar: #Consider empty cigar reads to be unmapped
+                positions.append(read.pos)
+                lengths.append(read.rlen)
+                seqs += read.seq
+                cigars += [val for step in read.cigar for val in step] + [-1, -1]
         return {
             'pos': np.asarray(positions, dtype=np.uint32),
             'len': np.asarray(lengths, dtype=np.uint16),
